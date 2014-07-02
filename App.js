7f9df8d824b0bf41c357126fff8f8b028f002c53
mqli <limanqing315@gmail.com>
2014-07-02 10:38:46 +0800

    update

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-define(["backbone"], function () {
+define(["backbone",'LayoutView'], function (Backbone, LayoutView) {
   return  Backbone.Router.extend({
     initialize: function () {
       this.route(":module", "module");
+      this.route("", "index");
     },
     module: function (module) {
       console.log(module)
+    },
+    index: function () {
+      console.log('index');
+      new LayoutView({
+        el: document.body
+      }).render()
     }
   });
 });
---
+++ LayoutView.js
@@ -0,0 +1,10 @@
+define(["backbone"], function (Backbone) {
+  return  Backbone.View.extend({
+    initialize: function () {
+      console.log('initialize laylout');
+    },
+    render: function () {
+      
+    }
+  });
+});
